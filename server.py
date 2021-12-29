d6e928803853a7d0a2ca3515bf9bfad3cd9404eb
hanna <hannapark56@sookmyung.ac.kr>
2021-12-29 19:06:24 +0900

    Flask 연동 및 추천 결과 구현

---
+++ server.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request
+import pandas as pd
+
+recommendation_df = pd.read_json('./data/song_id2playlist_word2vec.json', typ = 'frame')
+song_meta_df = pd.read_json('./data/song_meta_data_v3.json', typ = 'frame')
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+    if request.method == 'POST':
+        song = request.form['song_name']
+        song_id = int(song_meta_df[song_meta_df['song_name'] == song]["id"])
+        song_list = list(recommendation_df[song_id])
+        result = []
+        for i in range(len(song_list)):
+            result.append(song_meta_df[song_meta_df['id']==song_list[i]]["song_name"].values[0])
+
+        return render_template('index.html', output_song_list=result)
+
+if __name__ == '__main__':
+   app.run(debug = True)
